c7e5e28ea5f0a8fa044fe7755ada2a0397077d07
Karina Dalca <karinadalca@gmail.com>
2018-07-24 00:52:57 -0700

    makeArrayConsecutive2.js

---
+++ makeArrayConsecutive2.js
@@ -0,0 +1,23 @@
+// Ratiorg got statues of different sizes as a present from CodeMaster for his birthday, each statue having an non-negative integer size. Since he likes to make things perfect, he wants to arrange them from smallest to largest so that each statue will be bigger than the previous one exactly by 1. He may need some additional statues to be able to accomplish that. Help him figure out the minimum number of additional statues needed.
+
+// Example
+
+// For statues = [6, 2, 3, 8], the output should be
+// makeArrayConsecutive2(statues) = 3.
+
+// Ratiorg needs statues of sizes 4, 5 and 7.
+
+function makeArrayConsecutive2(statues) {
+    var sorted = statues.sort(function(a, b) { return a - b });
+    var current = sorted[0];
+    var needed = 0;
+    
+    for(var i = 1; i < sorted.length ; i++){
+        if(sorted[i] !== current+1) {
+            needed = needed+(sorted[i]-current-1);
+        }
+        current=sorted[i];
+    }
+    
+    return needed;
+}
